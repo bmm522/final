38ac8b64baabb5138eff8a5acadaa94a6e2f8d5a
JiIn Kim <rlawldls11@naver.com>
2022-10-05 15:05:56 +0900

    비밀번호 변경구현(백) 컨트롤러, DTO생성

---
+++ src/main/java/com/LoginAndSignUp/ChangePwd/NewPwd/DTO/NewPwdDTO.java
@@ -0,0 +1,11 @@
+package com.LoginAndSignUp.ChangePwd.NewPwd.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NewPwdDTO {
+	private String newPwd;
+	private String checkNewPwd;
+}
