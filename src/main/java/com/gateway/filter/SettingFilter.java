ffd4164838d0299172870dd4475376eaabf33245
JiIn Kim <rlawldls11@naver.com>
2022-10-19 09:13:18 +0900

    feat<SettingFilter> : Add Authorized

--- src/main/java/com/gateway/filter/SettingFilter.java
+++ src/main/java/com/gateway/filter/SettingFilter.java
@@ -1,12 +1,17 @@
 package com.gateway.filter;
 
+import java.util.Objects;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -18,9 +23,43 @@ public class SettingFilter extends AbstractGatewayFilterFactory<SettingFilter.Co
 	
 	@Override
 	public GatewayFilter apply(Config config) {
-
-		return null;
+		return ((exchange, chain) ->{
+			ServerHttpRequest request = exchange.getRequest(); // Pre Filter로 적용
+			ServerHttpResponse response = exchange.getResponse(); // Post Filter는 Response로 받아오면 된다.
+			HttpHeaders requestHeaders = request.getHeaders();
+			settingForCors(requestHeaders, response.getHeaders());
+			
+			if(!requestHeaders.containsKey("token")) { 			//토큰 존재하지 않을때
+				return notiUnAuthorized(exchange); 
+			}
+			String token = Objects.requireNonNull( 
+									  request.getHeaders().get("token"))
+									  .get(0); // 빠른 오류 확인 위해서 requiredNonNull 사용.
+			
+			if(!token.equals("A.B.C")) { 			//토큰 일치하지 않을때
+				return notiUnAuthorized(exchange);
+			}
+			
+			return chain.filter(exchange);
+		});
 	}
+	
+
+
+	private void settingForCors(HttpHeaders requestHeaders, HttpHeaders responseHeaders) {
+		
+		
+		
+	}
+
+	private Mono<Void> notiUnAuthorized(ServerWebExchange exchange) {
+		ServerHttpResponse response = exchange.getResponse();
+		
+		response.setStatusCode(HttpStatus.UNAUTHORIZED);
+		return response.setComplete();
+	}
+	
+	
 	
 	public static class Config{
 		
