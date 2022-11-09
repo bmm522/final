1abb72e36c7ac7c41dc20424bcbfac93de79ba9e
bmm522 <rlawldls11@naver.com>
2022-11-09 19:52:54 +0900

    Add : loginPage

--- JS/login.js
+++ JS/login.js
@@ -1,34 +1,180 @@
-const nameBtn = document.querySelector('.userName');
-const passwordBtn = document.querySelector('.userPassword');
-const loginBtn = document.querySelector('.loginBtn');
-
-function goToMain(){
-    location.href = "main.html"
-}
-
-nameBtn.addEventListener('keyup', function(){
-    let nameLength = nameBtn.value.length;
-    let passwordLength = passwordBtn.value.length;
-
-    if(nameLength >= 1 && passwordLength >= 5 && nameBtn.value.indexOf('@') !== -1 ){
-        loginBtn.style.backgroundColor = "#0095f6"; 
-        loginBtn.addEventListener('click', goToMain)       
+// Elements
+const el = {
+    signUpHome: document.getElementById('sign-up'),
+    signInHome: document.getElementById('sign-in'),
+    btnHome: document.querySelector('.btn-back'),
+    pageMain: document.querySelector('.main'),
+    pageHome: document.querySelector('.home'),
+    pageSignUp: document.querySelector('.sign-up'),
+    formArea: document.querySelector('.form-area'),
+    sideSignLeft: document.querySelector('.signup-left'),
+    sideSignRight: document.querySelector('.signup-right'),
+    formSignUp: document.querySelector('.form-area-signup'),
+    formSignIn: document.querySelector('.form-area-signin'),
+    linkUp: document.querySelector('.link-up'),
+    linkIn: document.querySelector('.link-in'),
+    btnSignUp: document.querySelector('.btn-up'),
+    btnSignIn: document.querySelector('.btn-in'),
+    labels: document.getElementsByTagName('label'),
+    inputs: document.getElementsByTagName('input'),
+  };
+  
+  
+  // ADD Events
+  // Show the page Sign Up
+  el.signUpHome.addEventListener('click', function(e) {
+    showSign(e, 'signup');
+  });
+  el.linkUp.addEventListener('click', function(e) {
+    showSign(e, 'signup');
+  });
+  
+  // Show the page sign in
+  el.signInHome.addEventListener('click', function(e) {
+    showSign(e, 'signin');
+  });
+  el.linkIn.addEventListener('click', function(e) {
+    showSign(e, 'signin');
+  });
+  el.btnSignUp.addEventListener('click', function(e) {
+    showSign(e, 'signin');
+  });
+  
+  // Show the page Home
+  el.btnHome.addEventListener('click', showHome);
+  
+  
+  // Functions Events
+  // function to show screen Home
+  function showHome(event) {
+    
+    
+    setTimeout(function() {
+      el.sideSignLeft.style.padding = '0';
+      el.sideSignLeft.style.opacity = '0';
+      el.sideSignRight.style.opacity = '0';
+      el.sideSignRight.style.backgroundPositionX = '235%';
+  
+      el.formArea.style.opacity = '0';
+      setTimeout(function() {
+        el.pageSignUp.style.opacity = '0';
+        el.pageSignUp.style.display = 'none';
+        for (input of el.inputs)  {
+          input.value = '';
+        }
+      }, 900);
+  
+    }, 100);
+  
+    setTimeout(function() {
+      el.pageHome.style.display = 'flex';
+    },1100);
+    
+    setTimeout(function() {
+      el.pageHome.style.opacity = '1';
+    }, 1200);
+  
+  }
+  // function to show screen Sign up/Sign in
+  function showSign(event, sign) {
+  
+    if (sign === 'signup') {
+      el.formSignUp.style.display = 'flex';
+      el.formSignIn.style.opacity = '0';
+      setTimeout(function() {
+        el.formSignUp.style.opacity = '1';
+      }, 100);
+      el.formSignIn.style.display = 'none';
+  
     } else {
-        loginBtn.style.backgroundColor = "#b2dffc";
+      el.formSignIn.style.display = 'flex';
+      el.formSignUp.style.opacity = '0';
+      setTimeout(function() {
+        el.formSignIn.style.opacity = '1';
+      }, 100);
+      el.formSignUp.style.display = 'none';
     }
-});
+  
+    el.pageHome.style.opacity = '0';
+    setTimeout(function() {
+      el.pageHome.style.display = 'none';
+    }, 700);
+    
+    setTimeout(function() {
+      el.pageSignUp.style.display = 'flex';
+      el.pageSignUp.style.opacity = '1';
+      
+      setTimeout(function() {
+        el.sideSignLeft.style.padding = '20px';
+        el.sideSignLeft.style.opacity = '1';
+        el.sideSignRight.style.opacity = '1';
+        el.sideSignRight.style.backgroundPositionX = '230%';
+  
+        el.formArea.style.opacity = '1';
+      }, 10);
+  
+    }, 900);
+  }
+  
+  // Behavior of the inputs and labels
+  for (input of el.inputs) {
+    console.log(input)
+    input.addEventListener('keydown', function() {
+      this.labels[0].style.top = '10px';
+    });
+    input.addEventListener('blur', function() {
+      if (this.value === '') {
+        this.labels[0].style.top = '25px';
+      }
+    })
+  }
 
-passwordBtn.addEventListener('keyup', function(){
-    let nameLength = nameBtn.value.length;
-    let passwordLength = passwordBtn.value.length;
-
-    if(nameLength >= 1 && passwordLength >= 5 && nameBtn.value.indexOf('@') !== -1){
-        loginBtn.style.backgroundColor = "#0095f6"; 
-        loginBtn.addEventListener('click', goToMain)       
-    } else {
-        loginBtn.style.backgroundColor = "#b2dffc";
-    }
-});
 
 
+  function DaumAPI() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var addr = ''; // 주소 변수
+            var extraAddr = ''; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+            if(data.userSelectedType === 'R'){
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+                // 조합된 참고항목을 해당 필드에 넣는다.
+                document.getElementById("extraAddress").value = extraAddr;
+            
+            } else {
+                document.getElementById("extraAddress").value = '';
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('postcode').value = data.zonecode;
+            document.getElementById("address").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("detailAddress").focus();
+        }
+    }).open();
+}  
